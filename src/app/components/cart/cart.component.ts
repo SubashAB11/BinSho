c12bd00598f0d7b57c7b7662a213129e882d8205
SubossAB <subash.a.b123@gmail.com>
2025-08-07 10:49:46 +0530

    added shuffling feature for cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product.model';
-import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragEnd, DragDropModule } from '@angular/cdk/drag-drop';
 import { ProductComponent } from '../product/product.component';
 import { timer } from 'rxjs';
 import { AppService } from '../../services/app.service';
+import { MovableDirective } from '../../directives/movable.directive';
 
 @Component({
   selector: 'app-cart',
-  imports: [DragDropModule, ProductComponent],
+  imports: [DragDropModule, ProductComponent, MovableDirective],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-  cartItems: (Product | null)[] = [];
+  cartItems: Product[] = [];
 
   constructor(private appService: AppService) { }
 
@@ -20,19 +21,16 @@ export class CartComponent {
     return this.cartItems.reduce((sum, item) => sum + (item ? item.price : 0), 0);
   }
 
-  onDrop(event: CdkDragDrop<(Product | null)[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      const draggedItem = event.previousContainer.data[event.previousIndex];
-      this.addAnimation(draggedItem, event.item.data);
-      if (draggedItem) {
-        event.container.data.splice(event.currentIndex, 0, draggedItem);
-      }
+  onDrop(event: CdkDragDrop<Product[]>) {
+    if (event.previousContainer === event.container) return;
+    const draggedItem = event.previousContainer.data[event.previousIndex];
+    this.addAnimation(draggedItem, event.item.data);
+    if (draggedItem) {
+      event.container.data.splice(event.currentIndex, 0, draggedItem);
     }
   }
 
-  addAnimation(draggedItem: Product | null, side: string) {
+  addAnimation(draggedItem: Product, side: string) {
     const productElement = document.querySelector(`#product-${draggedItem?.id}`);
     const reload = side === 'left' ? 'reloading-left' : 'reloading-right';
     if (productElement) {
@@ -45,7 +43,7 @@ export class CartComponent {
     }
   }
 
-  trackByIndex(index: number, item: (Product | null)): number {
+  trackByIndex(index: number, item: Product): number {
     return index;
   }
 }
